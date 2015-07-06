d64464969cf768db12ea04e38844a574200f9dec
Prappo Prince <prappo.prince@gmail.com>
2015-07-06 23:13:28 +0600

    Delete config.php

--- lib/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-  (!defined('HOST')) ? define('HOST', 'localhost') : NULL;
-  (!defined('USER')) ? define('USER', 'root') : NULL;
-  (!defined('PASS')) ? define('PASS', '') : NULL;
-  (!defined('DB')) ? define('DB', 'vor') : NULL;
-?>
