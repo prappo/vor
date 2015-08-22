f8bb5af2426f05d66197d4491ecc487fbdd56f36
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2015-08-23 01:43:43 +0600

    Code beautified

--- index.php
+++ index.php
@@ -17,18 +17,20 @@ function customError($errno, $errstr) {
 
 //set_error_handler("customError");
 
-class R{
-function a($r,callable $c)
-{
-	$this->r[$r]=$c;
-}
-function e()
-{
-	$s=$_SERVER;
-	$i='PATH_INFO';
-	$p=isset($s[$i])?$s[$i]:'/';
-	$this->r[$p]();
-}
-}
+class R {
+
+	function a($r,callable $c)
+	{
+		$this->r[$r]=$c;
+	}
+
+	function e()
+		{
+			$s=$_SERVER;
+			$i='PATH_INFO';
+			$p=isset($s[$i])?$s[$i]:'/';
+			$this->r[$p]();
+		}
+	}
 
 require('lib/core.php');
