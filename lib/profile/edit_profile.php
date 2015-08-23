345eece72d7b15ec214873f982256964cc60b602
Mehedi Hassan <Mehedi5531@gmail.com>
2015-08-23 20:33:57 +0600

    fixing edit profile image upload errors

--- lib/profile/edit_profile.php
+++ lib/profile/edit_profile.php
@@ -1,138 +1,129 @@
 <script>
-  function empty() {
-    swal({   title: "Error!",   text: "You must enter Name",   type: "error",   confirmButtonText: "OK" , timer: 2500 });
-  }
+function empty() {
+    swal({
+        title: "Error!",
+        text: "You must enter Name",
+        type: "error",
+        confirmButtonText: "OK",
+        timer: 2500
+    });
+}
 
-  function success() {
-    swal({   title: "Updated!",   text: "Profile updated",   type: "success",   confirmButtonText: "OK" , timer: 1500 });
-  }
+function success() {
+    swal({
+        title: "Updated!",
+        text: "Profile updated",
+        type: "success",
+        confirmButtonText: "OK",
+        timer: 1500
+    });
+}
 
-  function error(){
-    swal({   title: "Oops...",   text: "Something went wrong!",   type: "error",   confirmButtonText: "OK" , timer: 1500 });
-  }
+function error(){
+    swal({
+        title: "Oops...",
+        text: "Something went wrong!",
+        type: "error",
+        confirmButtonText: "OK",
+        timer: 1500
+    });
+}
 
-  function checkInput() {
+function checkInput() {
     name = document.getElementById('name').value;
-
     if(name == '') {
-      empty();
-      return false;
+        empty();
+        return false;
     }
-  }
+}
 </script>
-
 <?php
-  require_once 'lib/imageResize.php';
+$rows = end((db_get_where('vor_admin', array('username' => $_SESSION['username']))));
+$image = $rows['image'];
+$image    = 'img/user/'.$image;
+$real_img = 'img/user/'.$rows['image'];
 
-  $rows = end((db_get_where('vor_admin', array('username' => $_SESSION['username']))));
-  $image = $rows['image'];
-
-  $image    = 'img/user/'.$image;
-  $real_img = 'img/user/'.$rows['image'];
-
-  if(!file_exists($image) || empty($rows['image'])) {
+if(!file_exists($image) || empty($rows['image'])) {
     $image = 'img/user/admin_1.png';
-  }
+}
 
-  if(isset($_POST['updateProfile'])) {
+if(isset($_POST['updateProfile'])) {
     if(empty($_POST['name'])) {
-      echo '<script>empty();</script>';
+        echo '<script>empty();</script>';
     } else {
-      $image_name = (!empty($_FILES['image']['name'])) ? $_FILES['image']['name'] : NULL;
-      $tmp_name   = (!empty($_FILES['image']['tmp_name'])) ? $_FILES['image']['tmp_name'] : NULL;
+        $check = save_user_image($_FILES['image'], [
+            'name'   => $_SESSION['username'],
+            'width'  => 140,
+            'height' => 140
+        ]);
 
-      $img_location = 'img/admin/'.$image_name;
+        $fields = array(
+            'full_name' => $_POST['name'],
+            'email'     => $_POST['email']
+        );
 
-      if(!empty($image_name)) {
-        $mime = getimagesize($tmp_name);
-
-        if(in_array('image', explode('/', $mime['mime']))) {
-          move_uploaded_file($tmp_name, $img_location);
-          
-          $image = new ImageResize($img_location);
-          $image->resize(140, 140);
-          $image->save();
+        $status = db_update('vor_admin', $fields, ['username' => $_SESSION['username']]);
+        if($status) {
+            echo '<script>success();</script>';
+            echo "<meta http-equiv='refresh' content='1;url='>";
+        } else {
+            echo '<script>error();</script>';
+            echo "<meta http-equiv='refresh' content='1;url='>";
         }
-      }
-
-      $fields = array(
-        'full_name' => $_POST['name'],
-        'email'     => $_POST['email']
-      );
-
-      $where = array(
-        'username' => $_SESSION['username']
-      );
-
-      if(!empty($image_name)) {
-        unlink($real_img);
-        $fields['image'] = $image_name;
-      }
-
-      $success = db_update('vor_admin', $fields, $where);
-
-      if($success) {
-        echo '<script>success();</script>';
-        echo "<meta http-equiv='refresh' content='1;url='>";
-      } else {
-        echo '<script>error();</script>';
-        echo "<meta http-equiv='refresh' content='1;url='>";
-      }
     }
-  }
+}
 ?>
 <div class="row">
-  <aside class="profile-nav col-lg-3">
-    <section class="panel">
-      <div class="user-heading round">
-        <a href="#">
-          <img src="<?php echo $image; ?>" alt="">
-        </a>
-        <h1><?php echo $rows['full_name']; ?></h1>
-        <p><?php echo $rows['email']; ?></p>
-      </div>
-      <ul class="nav nav-pills nav-stacked">
-        <li><a href="profile"> <i class="fa fa-user"></i> Profile</a></li>
-        <li><a href="change_password"> <i class="fa fa-lock"></i> Change Password</a></li>
-        <li class="active"><a href="edit_profile"> <i class="fa fa-edit"></i> Edit profile</a></li>
-      </ul>
-    </section>
-  </aside>
-  <aside class="profile-info col-lg-9">
-    <section>
-      <div class="panel panel-primary">
-        <div class="panel-heading"> Edit your profile information</div>
-        <div class="panel-body">
-          <div class="panel-body bio-graph-info">
-            <form class="form-horizontal" role="form" enctype="multipart/form-data" method="post" onsubmit="return checkInput()">
-              <div class="form-group">
-                <label class="col-lg-2 control-label">Name</label>
-                <div class="col-lg-6">
-                  <input type="text" class="form-control" id="name" placeholder="<?php echo $rows['full_name']; ?>" value="<?php echo $rows['full_name']; ?>" name="name">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-lg-2 control-label">Email</label>
-                <div class="col-lg-6">
-                  <input type="text" class="form-control" id="email" placeholder="<?php echo $rows['email']; ?>" value="<?php echo $rows['email']; ?>" name="email">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-lg-2 control-label">Change Avatar</label>
-                <div class="col-lg-6">
-                    <input type="file" name="image" class="file-pos" id="exampleInputFile" accept="image/*">
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="col-lg-offset-2 col-lg-10">
-                  <button type="submit" class="btn btn-danger" name="updateProfile">Save</button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </section>
+    <aside class="profile-nav col-lg-3">
+        <section class="panel">
+            <div class="user-heading round">
+                <a href="#">
+                    <img src="<?php echo $image; ?>" alt="">
+                </a>
+                <h1><?php echo $rows['full_name']; ?></h1>
+                <p><?php echo $rows['email']; ?></p>
+            </div>
+            <ul class="nav nav-pills nav-stacked">
+                <li><a href="profile"> <i class="fa fa-user"></i> Profile</a></li>
+                <li><a href="change_password"> <i class="fa fa-lock"></i> Change Password</a></li>
+                <li class="active"><a href="edit_profile"> <i class="fa fa-edit"></i> Edit profile</a></li>
+            </ul>
+        </section>
     </aside>
-  </div>
-
-  
+    <aside class="profile-info col-lg-9">
+        <section>
+            <div class="panel panel-primary">
+                <div class="panel-heading"> Edit your profile information</div>
+                <div class="panel-body">
+                    <div class="panel-body bio-graph-info">
+                        <form class="form-horizontal" role="form" enctype="multipart/form-data" method="post" onsubmit="return checkInput()">
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">Name</label>
+                                <div class="col-lg-6">
+                                    <input type="text" class="form-control" id="name" placeholder="<?php echo $rows['full_name']; ?>" value="<?php echo $rows['full_name']; ?>" name="name">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">Email</label>
+                                <div class="col-lg-6">
+                                    <input type="text" class="form-control" id="email" placeholder="<?php echo $rows['email']; ?>" value="<?php echo $rows['email']; ?>" name="email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">Change Avatar</label>
+                                <div class="col-lg-6">
+                                    <input type="file" name="image" class="file-pos" id="exampleInputFile" accept="image/*">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-lg-offset-2 col-lg-10">
+                                    <button type="submit" class="btn btn-danger" name="updateProfile">Save</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </aside>
+</div>
